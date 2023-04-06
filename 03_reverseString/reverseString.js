2af929c8f38ca60993cfd167db5b5b629682deff
acpasnello <anthony.pasnello@gmail.com>
2023-04-06 16:29:03 -0400

    Complete Exercise 03. reverseString function passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = [];
+    for (let i = string.length-1;i >= 0;i--) {
+        reverse.push(string[i]);
+    }
+    reverse = reverse.join("");
 
+    return reverse;
 };
 
 // Do not edit below this line
