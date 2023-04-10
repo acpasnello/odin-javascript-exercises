a84c440fbf5686828754d74c89d653f5fe78f5dd
acpasnello <anthony.pasnello@gmail.com>
2023-04-10 14:42:06 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+    let a;
+    let b;
+    if (x > y) {
+        a = y;
+        b = x;
+    } else {
+        a = x;
+        b = y;
+    }
 
+    let sum = 0
+    for (let i = a;i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
