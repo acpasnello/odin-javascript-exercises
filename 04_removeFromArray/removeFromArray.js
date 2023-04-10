3f33e479ad51717291cb757e654bb1f05067a807
acpasnello <anthony.pasnello@gmail.com>
2023-04-10 11:40:06 -0400

    Complete exercise 04 remove from Array Remove items from submitted array using rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    const args = toRemove;
+    // const numArgs = toRemove.length();
+    let inputArray = array;
+    for (const arg of args) {
+        let index = inputArray.indexOf(arg)
+        if (index != -1) {
+            inputArray.splice(index, 1);
+        }
+    }
 
+    return inputArray
 };
 
 // Do not edit below this line
