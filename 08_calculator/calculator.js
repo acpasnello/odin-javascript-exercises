2758308a042a72cc3ae2f2d2ec86e5f8172bb1c1
acpasnello <anthony.pasnello@gmail.com>
2023-04-25 23:47:17 -0400

    Complete exercise 08_Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let result = 0;
+  for (let i = 0;i < x.length;i++) {
+    result += x[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(x) {
+  let result = x[0];
+  for (let i = 1;i < x.length;i++) {
+    result *= x[i]
+  }
 
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x > 1) {
+    return x * factorial(x - 1)
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
