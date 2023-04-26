bc6c410b9befe798453002e545aa858bfa4a0377
acpasnello <anthony.pasnello@gmail.com>
2023-04-26 16:58:19 -0400

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS'
+    }
+    // While length of array <= requested index, generate next value
+    let array = [1, 1];
+    let start = 1;
+    while (array.length <= num) {
+        array.push(array[array.length-1] + array[array.length-2]);
+    }
+    return array[num-1];
 };
 
 // Do not edit below this line
