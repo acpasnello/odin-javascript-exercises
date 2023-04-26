4e26c7d0537464d32bd0c4f93d789ac3063675e7
acpasnello <anthony.pasnello@gmail.com>
2023-04-26 17:11:32 -0400

    Cover 0 condition for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,8 @@
 const fibonacci = function(num) {
-    if (num < 0) {
-        return 'OOPS'
-    }
+    if (num < 0) return 'OOPS';
+    if (count === 0) return 0;
     // While length of array <= requested index, generate next value
     let array = [1, 1];
-    let start = 1;
     while (array.length <= num) {
         array.push(array[array.length-1] + array[array.length-2]);
     }
