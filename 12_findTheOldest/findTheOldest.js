f3e113a7dcd286828728d2b34a6c7f44555f1e74
acpasnello <anthony.pasnello@gmail.com>
2023-04-28 12:21:22 -0400

    Complete exercise 12_findTheOldest using reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.reduce((previousValue, currentValue) => {
+        // Calculate age of current and previous
+        let currentAge = currentValue.yearOfDeath ? currentValue.yearOfDeath-currentValue.yearOfBirth : new Date().getFullYear() - currentValue.yearOfBirth;
+        let previousAge = previousValue.yearOfDeath ? previousValue.yearOfDeath-previousValue.yearOfBirth : new Date().getFullYear() - previousValue.yearOfBirth;
+        // Compare ages, assign older to 
+        let result = currentAge > previousAge ? currentValue : previousValue;
+        return result
+    })
+    return oldest;
 };
 
 // Do not edit below this line
